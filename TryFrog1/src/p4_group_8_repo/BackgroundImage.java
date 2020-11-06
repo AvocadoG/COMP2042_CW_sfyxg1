6f3af9264f5dc1e9f514ff4a9b6dd0815563043b
GohXinYee sfyxg1 <sfyxg1@nottingham.edu.my>
2020-11-06 22:53:01 +0800

    Remove class BackgroundImage and Digit from extending Actor

--- TryFrog1/src/p4_group_8_repo/BackgroundImage.java
+++ TryFrog1/src/p4_group_8_repo/BackgroundImage.java
@@ -1,14 +1,15 @@
 package p4_group_8_repo;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class BackgroundImage extends Actor{
+public class BackgroundImage extends ImageView{
 
-	@Override
+	/*@Override
 	public void act(long now) {
 		
 		
-	}
+	}*/
 	
 	public BackgroundImage(String imageLink) {
 		setImage(new Image(imageLink, 613, 800, false, true));
--- TryFrog1/src/p4_group_8_repo/Digit.java
+++ TryFrog1/src/p4_group_8_repo/Digit.java
@@ -1,15 +1,17 @@
 package p4_group_8_repo;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class Digit extends Actor{
+public class Digit extends ImageView{
 	int dim;
 	Image im1;
-	@Override
+	
+	/*@Override
 	public void act(long now) {
 		// TODO Auto-generated method stub
 		
-	}
+	}*/
 	
 	public Digit(int n, int dim, int x, int y) {
 		im1 = new Image("file:src/froggertextures/"+n+".png", dim, dim, true, true);
