6f4981b311dc6c1c9eb1eb5ac5b2fbe81ed653e8
GohXinYee sfyxg1 <sfyxg1@nottingham.edu.my>
2020-11-07 21:58:24 +0800

    Added ImageGenerator Interface

---
+++ TryFrog1/src/p4_group_8_repo/ImageGenerator.java
@@ -0,0 +1,11 @@
+package p4_group_8_repo;
+
+import javafx.scene.image.Image;
+
+public interface ImageGenerator{
+
+	Image createImage(String ImageLink);
+	
+	//createImage(String imagelink){
+	//img = new Image("imagelink",width,height,true,true);
+}
