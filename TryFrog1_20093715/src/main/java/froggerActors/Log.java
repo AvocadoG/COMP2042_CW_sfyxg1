6ce46f23acde808aaceb6c310016b74a5b425f5a
GohXinYee sfyxg1 <sfyxg1@nottingham.edu.my>
2020-12-01 13:23:21 +0800

    Add gradle build file

---
+++ TryFrog1_20093715/src/main/java/froggerActors/Log.java
@@ -0,0 +1,104 @@
+package froggerActors;
+
+import javafx.scene.image.Image;
+
+/**
+ * <b>Represents the logs floating on the river in Frogger Game.<br>
+ * 2 types of logs : Log with bombs and Log without bombs. If Animal jumps onto logs with bombs, it will be killed, drown into the river and die.
+ * {@code Log} is also an {@link froggerActors.Actor}</b>
+ */
+public class Log extends Actor {
+
+	/** moving speed of {@code Log}*/
+	private double speed;
+	/** Image of {@code Log}*/
+	private Image logImg;
+	private int imagewidth,imageheight;
+	/** determine if {@code Log} has bombs*/
+	private boolean hasbomb;
+	
+	/**
+	 * This constructor will create a {@code Log} object for the game
+	 * @param imageLink the image link for how the {@code Log} object will be displayed in the game
+	 * @param size the size of the {@code Log} object 
+	 * @param xpos the x position of the {@code Log} object in the game scene
+	 * @param ypos the y position of the {@code Log} object in the game scene
+	 * @param s the moving speed of the {@code Log} object in the game
+	 */
+	public Log(String imageLink, int size, int xpos, int ypos, double s) {
+		
+		this.imageheight=size;
+		this.imagewidth=size;
+		logImg = createImage(imageLink);
+		setImage(logImg);
+		setX(xpos);
+		setY(ypos);
+		speed = s;
+		
+	}
+	
+	/**
+	 * This constructor will create a {@code Log} object with bomb for the game
+	 * @param imageLink the image link for how the {@code Log} object will be displayed in the game
+	 * @param size the size of the {@code Log} object 
+	 * @param xpos the x position of the {@code Log} object in the game scene
+	 * @param ypos the y position of the {@code Log} object in the game scene
+	 * @param s the moving speed of the {@code Log} object in the game
+	 * @param bomb whether the {@code Log} has bombs
+	 */
+	public Log(String imageLink, int size, int xpos, int ypos, double s, boolean bomb) {
+		
+		this(imageLink,size,xpos,ypos,s);
+		hasbomb=bomb;
+		
+	}
+	
+	/**
+	 * Defines how {@code Log} object will behave in the game.<br>
+	 * {@code Log} object will move continuously in the game, either from left to right or from right to left.
+	 * 
+	 */
+	@Override
+	public void act(long now) {
+		move(speed , 0);
+		if (getX()>600 && speed>0)
+			setX(-180);
+		if (getX()<-300 && speed<0)
+			setX(700);
+	}
+
+	/**
+	 * Checks if the {@code Log} object is moving towards the left.
+	 * @return boolean <b>true</b> if the {@code Log} object is moving to the left.
+	 */
+	public boolean goingLeft() {
+		return speed < 0;
+	}
+	
+	/**
+	 * Checks if the {@code Log} object has bombs
+	 * @return boolean <b>true</b> if the {@code Log} object has bombs.
+	 * see also {@link Log#hasbomb}
+	 */
+	public boolean hasbomb() {
+		return hasbomb;
+	}
+
+	/** Retrieves the moving speed of {@code Log} object
+	 * @return speed of {@code Log} object*/
+	public double getSpeed() {
+		return speed;
+	}
+	
+	/**
+	 * to create Images for {@code Log} object with display.<br>
+	 * Used exclusively by {@code Log} object only.
+	 * @param ImageLink the link of the image to be created
+	 * @return {@code Image} a created image for {@code Log}
+	 */
+	private Image createImage(String ImageLink) {
+		// TODO Auto-generated method stub
+		Image img = new Image(ImageLink, this.imagewidth,this.imageheight,true,true);
+		return img;
+	}
+}
---
+++ TryFrog1_20093715/src/main/java/froggerActors/Monster.java
@@ -0,0 +1,74 @@
+package froggerActors;
+
+import javafx.scene.image.Image;
+
+/**
+ * <b>Represents the Monster in the game. It will eat {@link froggerAnimal_Actions.Animal}.
+ * <br>
+ * {@code Monster} is also an {@link froggerActors.Actor}</b>
+ * 
+ * @author GohXinYee
+ *
+ */
+public class Monster extends Actor{
+
+	private double speed;
+	/** Image for {@code Monster}*/
+	private Image monsterImg1,monsterImg2;
+	private int imgwidth,imgheight;
+	
+	/**
+	 * This constructor will create a {@code Monster} object with display
+	 * @param size size of {@code Monster}
+	 * @param posX x position of {@code Monster} in the game scene
+	 * @param posY y position of {@code Monster} in the game scene
+	 * @param s moving speed of {@code Monster} in the game
+	 */
+	public Monster(int size, int posX, int posY, double s) {
+
+		this.imgwidth=size;
+		this.imgheight=size;
+		monsterImg1 = createImage("file:src/froggertextures/monster1.png");
+		monsterImg2 = createImage("file:src/froggertextures/monster2.png");
+		setImage(monsterImg1);
+		setX(posX);
+		setY(posY);
+		speed=s;
+		
+		
+	}
+
+
+	/**
+	 * Defines how {@code Monster} will act in the game.<br>
+	 * Monster will move horizontally, back and forth in the game, changing its color in between.
+	 */
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		move(speed,0);
+		if(getX()>600 && speed>0) {
+			setX(600);
+			speed=-speed;
+			setImage(monsterImg2);
+		}
+		if(getX()<0 && speed<0) {
+			setX(0);
+			speed=-speed;
+			setImage(monsterImg1);
+		}
+	}
+
+	/**
+	 * to create Images for {@code Monster} object.<br>
+	 * Used exclusively by {@code Monster} object only.
+	 * @param ImageLink the link of the image to be created
+	 * @return {@code Image} a created image for {@code Monster}
+	 */
+	private Image createImage(String ImageLink) {
+		// TODO Auto-generated method stub
+		Image img = new Image(ImageLink, this.imgwidth, this.imgheight,true,true);
+		return img;
+	}
+
+}
